82ff14d6f360f2113529b3745163e0d4eefcf840
jamesalzuk <james.baxter@alzheimersresearchuk.org>
2019-11-29 16:22:35 +0000

    add create_app function

--- application/__init__.py
+++ application/__init__.py
@@ -1,2 +1,23 @@
+import os
+from flask import Flask
+
 def create_app():
-	pass
+	"""
+	Create instance of flask application
+	
+	Returns
+	-------
+	app : Flask
+
+	"""
+	app = Flask(__name__)
+	app.config.from_object(os.environ['APP_SETTINGS'])
+
+	return app
+
+def main():
+	app = create_app()
+	app.run()
+
+if __name__ == '__main__':
+	main()
